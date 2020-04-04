8b373a6c4d11f02aac812b7e99ba872168f570e9
Jim <jimlotruglio@yahoo.com>
2020-04-04 13:12:46 -0600

    added dates to experiences

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_04_045556) do
+ActiveRecord::Schema.define(version: 2020_04_04_185828) do
 
   create_table "destinations", force: :cascade do |t|
     t.string "name"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2020_04_04_045556) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "destination_id"
+    t.datetime "date"
   end
 
   create_table "hotels", force: :cascade do |t|
---
+++ scratch.md
@@ -0,0 +1,6 @@
+#Create dates for experiences
+
+#You may want to separate dates and times for hotels too so check-in time is not required. But also maybe not. I dunno.
+
+#Make a menu bar of some kind to navigate easier.
+#fix logout being above add hotel and experience on destination page.
