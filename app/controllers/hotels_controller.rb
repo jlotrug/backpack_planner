01df4df8c868edf1c073c17da8161154f7a8f75a
Jim <jimlotruglio@yahoo.com>
2020-04-03 23:09:31 -0600

    built out basic hotel and experience create actions

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -1,10 +1,14 @@
 class HotelsController < ApplicationController
 
   def new
+    @destination = Destination.find(params[:destination_id])
+    @hotel = @destination.hotels.build
   end
 
   def create
-
+    @hotel = Hotel.new(hotel_params)
+    binding.pry
+    redirect_to hotel_path(@hotel)
   end
 
   def show
@@ -12,6 +16,12 @@ class HotelsController < ApplicationController
 
   def destroy
 
+  end
+
+  private
+
+  def hotel_params
+    params.require(:hotel).permit(:name, :paid, :price, :location, :checkin, :checkout, :destination_id )
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_04_042409) do
+ActiveRecord::Schema.define(version: 2020_04_04_045556) do
 
   create_table "destinations", force: :cascade do |t|
     t.string "name"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2020_04_04_042409) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "destination_id"
+    t.string "location"
   end
 
   create_table "trips", force: :cascade do |t|
