5258cda7f6830542b6ff42f25d9219962c3131c8
Jim <jimlotruglio@yahoo.com>
2020-04-28 16:27:27 -0600

    added dates to display dates helper

--- app/helpers/trips_helper.rb
+++ app/helpers/trips_helper.rb
@@ -3,6 +3,8 @@ module TripsHelper
   def display_dates(trip)
     if trip.start == nil && trip.end == nil
       "(add dates)"
+    else
+      "#{trip.start.strftime('%B %d %Y')} - #{trip.end.strftime('%B %d %Y')}"
     end
   end
 
