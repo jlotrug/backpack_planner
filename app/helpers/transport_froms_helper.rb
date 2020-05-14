b6d7a25018b3122f4a8d5826959ccdeb5336c9d2
Jim <jimlotruglio@yahoo.com>
2020-05-14 17:13:39 -0600

    created helpers for transport to and from displays

--- app/helpers/transport_froms_helper.rb
+++ app/helpers/transport_froms_helper.rb
@@ -1,2 +1,15 @@
 module TransportFromsHelper
+
+  def display_transport_from(destination)
+      !!destination.transport_from
+  end
+
+  def display_transport_from_dates(transport)
+    #binding.pry
+    if transport == nil
+      "(add date and time)"
+    else
+      "#{transport.strftime('%B %d %Y')} at #{transport.strftime('%l:%M%p')}"
+    end
+  end
 end
